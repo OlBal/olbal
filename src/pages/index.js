5963048d9859c1a430a0bb36ee955e1f5e531e62
Oliver Ballon <oliverballon1@gmail.com>
2020-10-04 18:08:22 +0100

    styled hamburger menu component added and working, some layout established

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import "../styles/styles.scss"
+import NavBar from "../components/Nav/NavBar"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <NavBar />
+    </>
+  )
 }
