c7b7f2da4745051db82d8a5f011643c47ef7a97a
Oliver Ballon <oliverballon1@gmail.com>
2020-10-22 22:02:29 +0100

    package Json edited, github links added to graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,6 +33,7 @@ export const pageQuery = graphql`
             tags
             position
             external
+            github
             category
             screenshot {
               childImageSharp {
